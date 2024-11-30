0cb0d6865c58a8745a0f905ce82e6710f475a773
fyambos <feliciteyambos@gmail.com>
2024-11-30 16:02:46 +0100

    feat: Add sign out method.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, signOut, User } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: User | null = null;
   
+  constructor(private auth: Auth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.auth.onAuthStateChanged((user) => {
+      this.user = user;
+    });
+  }
+
+  signOutUser() {
+    signOut(this.auth)
+      .then(() => {
+        this.router.navigate(['/login']);
+        console.log('User signed out');
+      })
+      .catch((error) => {
+        console.error('Sign-out error:', error);
+      });
+  }
 }
