6ff3b22a54d196393b6ecb219e4be9f4a6c65fbe
Rosvadle MANFO <florientmanfo3@outlook.fr>
2024-12-01 18:05:23 +0100

    feat: Create task editing dialog component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTaskDialogComponent } from 'src/app/components/edit-task-dialog/edit-task-dialog.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private readonly _dialog: MatDialog,
+  ) {}
+  
+  ngOnInit(): void {
+    this.openDialog()
+  }
+
+  openDialog() {
+    const dialogRef = this._dialog.open(EditTaskDialogComponent, {
+      width: '500px',
+      data: {
+        title: 'Create Task',
+        users: [{ id: '1', name: 'John Doe', email: '' }, { id: '2', name: 'Jane Doe', email: '' }],
+        currentUser: { id: '1', name: 'John Doe', email: '' },
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
 
 }
