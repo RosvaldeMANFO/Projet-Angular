54517957988e8073adf70634d8152c9ceeda049a
Rosvadle MANFO <florientmanfo3@outlook.fr>
2024-12-10 19:01:19 +0100

    Fix: Fix category badge color

--- src/app/directives/apply-color.directive.ts
+++ src/app/directives/apply-color.directive.ts
@@ -11,5 +11,24 @@ export class ApplyColorDirective implements OnInit {
 
   ngOnInit() {
     this.renderer.setStyle(this.el.nativeElement, 'color', this.appApplyColor);
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', this.hexToRgba(this.appApplyColor, 0.5));
+  }
+
+  private hexToRgba(hex: string, opacity: number): string {
+    let r = 0, g = 0, b = 0;
+
+    if (hex.length == 4) {
+      r = parseInt(hex[1] + hex[1], 16);
+      g = parseInt(hex[2] + hex[2], 16);
+      b = parseInt(hex[3] + hex[3], 16);
+    }
+
+    else if (hex.length == 7) {
+      r = parseInt(hex[1] + hex[2], 16);
+      g = parseInt(hex[3] + hex[4], 16);
+      b = parseInt(hex[5] + hex[6], 16);
+    }
+
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
   }
 }
